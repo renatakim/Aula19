2612a3d50074552157a40a6381c2eeb8d1e2626b
Renata Kim <renata_kim@hotmail.com>
2022-06-30 19:35:05 -0300

    Adicionando exercicio 19 e gitignore

---
+++ Wpf.MVVM/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using Wpf.MVVM.ViewModels;
+
+namespace Wpf.MVVM
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            // Inicializa o projeto
+            base.Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ Wpf.MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace Wpf.MVVM.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        private string nomeCompleto;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                // Observa se o atributo/instância FirstName recebeu alguma atualização.
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
